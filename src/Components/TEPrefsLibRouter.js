5eb2b0df651810990359b5eea2d4693676c652e3
Fredrik Björeman <fredrik.bjoreman@timeedit.com>
2019-11-20 10:49:31 +0100

    DEV-3987: Added logout button when logged in.

--- src/Components/TEPrefsLibRouter.js
+++ src/Components/TEPrefsLibRouter.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
+import { Button } from 'antd';
+import { deleteToken } from '../Utils/tokenHelpers';
 
 // PAGES
 import LoginPage from '../Pages/Login';
@@ -18,6 +20,14 @@ const mapStateToProps = state => ({
 });
 
 const TEPrefsLibRouter = ({ authenticated }) => {
+  const logoutButton = authenticated ? (<Button size="small" onClick={
+    async () => {
+      window.tePrefsLibStore.dispatch({ type: 'LOGIN_FAILURE' });
+      await deleteToken();
+      window.location = '/login';
+    }}>
+    Log out
+  </Button>) : null;
   return (
     <Router>
       <BreadcrumbsWrapper />
@@ -27,6 +37,7 @@ const TEPrefsLibRouter = ({ authenticated }) => {
         <Route exact path="/forms/:formId" component={FormPage} />
         <Route exact path="/forms/:formId/:formInstanceId" component={FormInstancePage} />
       </Switch>
+      {logoutButton}
     </Router>
   );
 };
