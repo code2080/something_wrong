da83dc424a970405f60ed6a2bd563bb96ac4ce6c
Fredrik Björeman <fredrik.bjoreman@timeedit.com>
2020-04-15 10:58:35 +0200

    DEV-4750: Use EllipsisRenderer for acceptance comments.

--- src/Components/TableColumns/formSubmission.js
+++ src/Components/TableColumns/formSubmission.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import moment from 'moment';
-import { toProgressLabel, toAcceptanceLabel } from '../../Constants/teCoreProps.constants';
+import {
+  toProgressLabel,
+  toAcceptanceLabel
+} from '../../Constants/teCoreProps.constants';
 
 // COMPONENTS
 import SubmissionActionButton from './Components/SubmissionActionButton';
+import EllipsisRenderer from './Components/EllipsisRenderer';
 
 export const formSubmission = {
   NAME: {
@@ -17,40 +21,43 @@ export const formSubmission = {
     key: 'updatedAt',
     dataIndex: 'updatedAt',
     render: val => moment(val).format('YYYY-MM-DD'),
-    sorter: (a, b) => moment(a.updatedAt).valueOf() - moment(b.updatedAt).valueOf(),
+    sorter: (a, b) =>
+      moment(a.updatedAt).valueOf() - moment(b.updatedAt).valueOf()
   },
   SCOPED_OBJECT: {
     title: 'Scoped object',
     key: 'scopedObject',
     dataIndex: 'scopedObject',
     render: val => val || 'Not scoped',
-    sorter: (a, b) => a.scopedObject - b.scopedObject,
+    sorter: (a, b) => a.scopedObject - b.scopedObject
   },
   ACTION_BUTTON: {
     title: 'Actions',
     key: 'actions',
     dataIndex: null,
-    render: (_, formInstance) => <SubmissionActionButton formInstance={formInstance} />,
+    render: (_, formInstance) => (
+      <SubmissionActionButton formInstance={formInstance} />
+    )
   },
   SCHEDULING_PROGRESS: {
     title: 'Scheduling progress',
     key: 'schedulingProgress',
     dataIndex: 'teCoreProps.schedulingProgress',
     render: val => toProgressLabel(val),
-    sorter: (a, b) => a.localeCompare(b),
+    sorter: (a, b) => a.localeCompare(b)
   },
   ACCEPTANCE_STATUS: {
     title: 'Acceptance status',
     key: 'acceptanceStatus',
     dataIndex: 'teCoreProps.acceptanceStatus',
     render: val => toAcceptanceLabel(val),
-    sorter: (a, b) => a.localeCompare(b),
+    sorter: (a, b) => a.localeCompare(b)
   },
   ACCEPTANCE_COMMENT: {
     title: 'Acceptance comment',
     key: 'acceptanceComment',
     dataIndex: 'teCoreProps.acceptanceComment',
-    render: val => val || '',
-    sorter: (a, b) => a.localeCompare(b),
+    render: val => <EllipsisRenderer text={val} />,
+    sorter: (a, b) => a.localeCompare(b)
   }
 };
