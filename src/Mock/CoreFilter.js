ec3d0a7982484af60af5380bc533cea13d1f7de0
Fredrik Björeman <fredrik.bjoreman@timeedit.com>
2020-04-16 08:27:00 +0200

    Updated filter mock to reflect newer, cleaner result using ext id instead of internal id for selected categories.

--- src/Mock/CoreFilter.js
+++ src/Mock/CoreFilter.js
@@ -1,9 +1,6 @@
 export const coreFilter = {
-  selectedCategories: { 64: ['114 Ljunggården öster', '202 norr'] },
-  associatedObjects: [],
-  virtual: 2,
-  abstract: 2,
-  reserve: 1,
-  active: 1,
+  selectedCategories: {
+    'room.building': ['114 Ljunggården öster', '202 norr']
+  },
   searchString: 'de'
 };
