0a65375b5bdde9074d4eba8c7cb5f4faeb97f4e2
Fredrik Björeman <fredrik.bjoreman@timeedit.com>
2020-04-22 09:49:05 +0200

    Updated mock data for Core filters.

--- src/Mock/CoreFilter.js
+++ src/Mock/CoreFilter.js
@@ -1,6 +1,11 @@
 export const coreFilter = {
   selectedCategories: {
-    'room.building': ['114 Ljunggården öster', '202 norr']
+    'room.building': ['114 Ljunggården öster', '202 norr'],
+    'general.department': [
+      'Biochemistry (BIOC)',
+      'Business Administration (BADM)'
+    ],
+    'room.equipment': ['Computer Lab']
   },
   searchString: 'de'
 };
