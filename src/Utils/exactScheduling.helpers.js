55c7b6820d569ebaa0ef8b15df849febfb6678b3
Fredrik Björeman <fredrik.bjoreman@timeedit.com>
2020-04-23 16:07:15 +0200

    Map Core reservation result to SchedulingReturn.

--- src/Utils/exactScheduling.helpers.js
+++ src/Utils/exactScheduling.helpers.js
@@ -1,5 +1,15 @@
-import { activityValueTypes, activityValueTypeProps } from '../Constants/activityValueTypes.constants';
-import { TECoreFieldModel, TECoreObjectModel, TECoreReservationModel } from '../Models/TECoreReservation.model';
+import {
+  activityValueTypes,
+  activityValueTypeProps
+} from '../Constants/activityValueTypes.constants';
+import {
+  TECoreFieldModel,
+  TECoreObjectModel,
+  TECoreReservationModel
+} from '../Models/TECoreReservation.model';
+
+import { SchedulingReturn } from '../Models/SchedulingReturn.model';
+import { activityStatuses } from './activityStatuses.constants';
 
 const convertActivityValueToReservationProp = activityValue => {
   switch (activityValue.type) {
@@ -10,15 +20,39 @@ const convertActivityValueToReservationProp = activityValue => {
     default:
       break;
   }
-}
+};
 
-const convertValuesToReservationProps = activity => (activity.values || []).reduce((prev, activityValue) => ({
-  ...prev,
-  [activityValueTypeProps[activityValue.type].path]: [
-    ...prev[activityValueTypeProps[activityValue.type].path],
-    convertActivityValueToReservationProp(activityValue),
-  ],
-}), { objects: [], fields: [] });
+const convertValuesToReservationProps = activity =>
+  (activity.values || []).reduce(
+    (prev, activityValue) => ({
+      ...prev,
+      [activityValueTypeProps[activityValue.type].path]: [
+        ...prev[activityValueTypeProps[activityValue.type].path],
+        convertActivityValueToReservationProp(activityValue)
+      ]
+    }),
+    { objects: [], fields: [] }
+  );
+
+const parseTECoreResultToScheduleReturn = (teCoreReturn, callback) => {
+  const errorCode = teCoreReturn.failures[0]
+    ? teCoreReturn.failures[0].result.references[0]
+    : 0;
+  const errorMessage = teCoreReturn.failures[0]
+    ? teCoreReturn.failures[0].result.reservation
+    : '';
+  callback(
+    new SchedulingReturn({
+      status:
+        teCoreReturn.failures.length === 0
+          ? activityStatuses.SCHEDULED
+          : activityStatuses.FAILED,
+      reservationId: teCoreReturn.newIds[0],
+      errorCode,
+      errorMessage
+    })
+  );
+};
 
 export const scheduleActivityExact = (activity, teCoreScheduleFn, callback) => {
   /**
@@ -33,10 +67,14 @@ export const scheduleActivityExact = (activity, teCoreScheduleFn, callback) => {
     startTime,
     endTime,
     objects,
-    fields,
+    fields
   });
   /**
    * Call the external API
    */
-  return teCoreScheduleFn({ reservation, callback });
+  return teCoreScheduleFn({
+    reservation,
+    callback: teCoreResult =>
+      parseTECoreResultToScheduleReturn(teCoreResult, callback)
+  });
 };
