61535409af8b46e2976575dc0bc5c86c96f01f55
Andreas Kjellqvist <linco95@gmail.com>
2020-08-21 11:32:21 +0200

    DEV-5225: Refactored field and object mappings in activitytable columns

--- src/Components/ActivitiesTableColumns/ActivitiesTableColumns.js
+++ src/Components/ActivitiesTableColumns/ActivitiesTableColumns.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import _ from 'lodash';
 
 // COMPONENTS
 import BaseActivityCol from './BaseColumn/BaseActivityCol';
@@ -11,42 +12,25 @@ import { selectExtIdLabel } from '../../Redux/TE/te.selectors';
 
 export const createActivitiesTableColumnsFromMapping = mapping => [
   ...TimingColumns[mapping.timing.mode](mapping),
-  ...(Object.keys(mapping.objects) || []).reduce(
-    (objects, objectKey) => [
-      ...objects,
-      {
-        title: useSelector(state => selectExtIdLabel(state)('types', objectKey)),
-        key: objectKey,
-        dataIndex: null,
-        render: (_, activity) => (
-          <BaseActivityCol
-            activity={activity}
-            type="VALUE"
-            prop={objectKey}
-            mapping={mapping}
-          />
-        )
-      }
-    ],
-    []
-  ),
-  ...(Object.keys(mapping.fields) || []).reduce(
-    (fields, fieldKey) => [
-      ...fields,
-      {
-        title: useSelector(state => selectExtIdLabel(state)('fields', fieldKey)),
-        key: fieldKey,
-        dataIndex: null,
-        render: (_, activity) => (
-          <BaseActivityCol
-            activity={activity}
-            type="VALUE"
-            prop={fieldKey}
-            mapping={mapping}
-          />
-        )
-      }
-    ],
+  ...[
+    ...Object.keys(mapping.objects).map(objKey => ['types', objKey]),
+    ...Object.keys(mapping.fields).map(fieldKey => ['fields', fieldKey])
+  ].reduce((values, [field, extId]) => [
+    ...values,
+    {
+      title: useSelector(state => selectExtIdLabel(state)(field, extId)),
+      key: extId,
+      dataIndex: null,
+      render: (_, activity) => (
+        <BaseActivityCol
+          activity={activity}
+          type="VALUE"
+          prop={extId}
+          mapping={mapping}
+        />
+      )
+    }
+  ],
     []
   ),
   ...StaticColumns
