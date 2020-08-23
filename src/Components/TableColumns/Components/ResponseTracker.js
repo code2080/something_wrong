b8464625270f02af6ea8e37e6ee1ff7e691fc27b
Johan Hörnell <johan.c.hornell@gmail.com>
2020-08-23 21:55:17 +0200

    improved response counter

--- src/Components/TableColumns/Components/ResponseTracker.js
+++ src/Components/TableColumns/Components/ResponseTracker.js
@@ -6,27 +6,25 @@ import { Progress, Tooltip } from 'antd';
 import { formInstanceStatusTypes } from '../../../Constants/formInstanceStatuses.constants';
 
 const ResponseTracker = ({ responses }) => {
-  const total = responses[formInstanceStatusTypes.SENT] || 0;
-  const drafts = responses[formInstanceStatusTypes.DRAFT] || 0;
   const submissions = responses[formInstanceStatusTypes.SUBMITTED] || 0;
+  const totalCount = Object.keys(responses).reduce((tot, key) => tot + responses[key], 0);
   return (
-    <Tooltip
-      title={
-        total
-          ? `${submissions} submitted and ${drafts} saved as drafts of ${total} forms sent`
-          : 'Form has not been assigned to any recipients'
-      }
-      getPopupContainer={() => document.getElementById('te-prefs-lib')}
-    >
-      <Progress
-        percent={total ? (drafts / total) * 100 : 0}
-        successPercent={total ? (submissions / total) * 100 : 0}
-        size="small"
-        format={() => 
-          total ? `${submissions}/${total}` : `${submissions}`
+    <div style={{ marginRight: '8px' }}>
+      <Tooltip
+        title={
+          totalCount > 0
+            ? `${submissions} submitted out of ${totalCount} forms sent or created`
+            : 'Form has not been assigned to any recipients'
         }
-      />
-    </Tooltip>
+        getPopupContainer={() => document.getElementById('te-prefs-lib')}
+      >
+        <Progress
+          successPercent={(submissions / totalCount) * 100}
+          size="small"
+          format={() => `${submissions}/${totalCount}`}
+        />
+      </Tooltip>
+    </div>
   );
 };
 
