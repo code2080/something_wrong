1e9948b92d7b0303b27f7e30bce6d3217fa82a31
Andreas Kjellqvist <linco95@gmail.com>
2020-09-18 10:13:38 +0200

    Implemented model for object requests

--- src/Redux/ObjectRequests/ObjectRequests.reducer.js
+++ src/Redux/ObjectRequests/ObjectRequests.reducer.js
@@ -2,12 +2,16 @@ import _ from 'lodash';
 import * as types from './ObjectRequests.actionTypes';
 import { initialState } from './ObjectRequests.initialState';
 
+// MODELS
+import ObjectRequest from '../../Models/ObjectRequest.model';
+
+
 export default (state = initialState, action) => {
   switch (action.type) {
     case types.FETCH_OBJECT_REQUESTS_SUCCESS: {
       return {
         ...state,
-        list: action.payload.requests,
+        list: action.payload.requests.reduce((reqs, req) => [...reqs, new ObjectRequest(req)]),
       };
     }
 
