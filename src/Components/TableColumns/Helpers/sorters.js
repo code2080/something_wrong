ba50973ec48e95b965caf7b620f4032a8be4607f
Andreas Kjellqvist <linco95@gmail.com>
2020-10-05 16:05:40 +0200

    Null is now sorted last if sorted alphabetically

--- src/Components/TableColumns/Helpers/sorters.js
+++ src/Components/TableColumns/Helpers/sorters.js
@@ -1,4 +1,4 @@
 import moment from 'moment';
 
-export const sortAlpha = (a, b) => (a == null) - (b == null) || -(a > b) || +(a < b);
+export const sortAlpha = (a, b) => (b == null) - (a == null) || -(a > b) || +(a < b);
 export const sortTime = (a, b) => (a == null) - (b == null) || -(moment(a).valueOf() > moment(b).valueOf()) || +(moment(a).valueOf() < moment(b).valueOf());
