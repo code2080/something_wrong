be40e3d2159262e816df8d8f8c0b1ccc80f4c0dd
Andreas Kjellqvist <linco95@gmail.com>
2020-10-05 17:21:03 +0200

    DEV-5347: Showing correct default panel in extra settings on table section

--- src/Components/Sections/SectionExtra.js
+++ src/Components/Sections/SectionExtra.js
@@ -19,7 +19,7 @@ const panels = {
 
 const SectionExtra = ({ formId, formInstanceId, section, sectionType }) => {
   const defaultActiveKey = useMemo(() => {
-    if (sectionType === SECTION_CONNECTED || sectionType === SECTION_CONNECTED)
+    if (sectionType === SECTION_CONNECTED || sectionType === SECTION_TABLE)
       return [panels.SELECT_SETTINGS];
     if (section.calendarSettings)
       return [panels.CALENDAR_SETTINGS];
