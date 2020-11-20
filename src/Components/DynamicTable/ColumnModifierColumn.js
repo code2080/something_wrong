877712f553f361314c83a54d4ae54ed001044b14
Chuong Tran <chuong14011991@gmail.com>
2020-11-20 19:26:43 +0700

    DEV-5896 Fix table issue after resized

--- src/Components/DynamicTable/ColumnModifierColumn.js
+++ src/Components/DynamicTable/ColumnModifierColumn.js
@@ -4,6 +4,8 @@ import { Icon } from 'antd';
 export const columnModifierColumn = onModifyCols => ({
   title: <Icon type="plus-circle" theme="filled" style={{ fontSize: '0.8rem' }} />,
   dataIndex: null,
+  fixedWidth: 34,
+  width: 34,
   key: 'modifyColumns',
   onHeaderCell: col => ({
     onClick: () => onModifyCols(),
