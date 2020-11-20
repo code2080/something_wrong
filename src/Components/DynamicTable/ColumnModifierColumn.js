b09875b46c4795d62f31809b475e512e0731f78a
Fredrik Björeman <fredrik.bjoreman@timeedit.com>
2020-11-20 15:27:25 +0100

    Revert "DEV-5896 Fix table issue after resized"
    This reverts commit 877712f553f361314c83a54d4ae54ed001044b14.

--- src/Components/DynamicTable/ColumnModifierColumn.js
+++ src/Components/DynamicTable/ColumnModifierColumn.js
@@ -4,8 +4,6 @@ import { Icon } from 'antd';
 export const columnModifierColumn = onModifyCols => ({
   title: <Icon type="plus-circle" theme="filled" style={{ fontSize: '0.8rem' }} />,
   dataIndex: null,
-  fixedWidth: 34,
-  width: 34,
   key: 'modifyColumns',
   onHeaderCell: col => ({
     onClick: () => onModifyCols(),
