abaaab94022d54bb9b8076b65d3ae29d87f22fbb
Chuong Tran <chuong14011991@gmail.com>
2021-02-06 10:39:35 +0700

    DEV-7015 Update TimePicker value

--- src/Components/Elements/TimePicker.js
+++ src/Components/Elements/TimePicker.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 import { Icon } from 'antd';
@@ -7,16 +7,26 @@ import { TIME_FORMAT } from '../../Constants/common.constants';
 // STYLES
 import './Pickers.scss';
 
-const TimePicker = ({ value }) => (
-  <div className="picker--wrapper">
-    <div className="icon--wrapper">
-      <Icon type="clock-circle" />
+const TimePicker = ({ value }) => {
+  const _value = useMemo(() => {
+    if (value === null || value === undefined) return 'N/A';
+    if (typeof value === 'string') return moment(value).format(TIME_FORMAT);
+    return moment()
+      .startOf('day')
+      .add(value, 'minutes')
+      .format(TIME_FORMAT);
+  }, [value]);
+  return (
+    <div className="picker--wrapper">
+      <div className="icon--wrapper">
+        <Icon type="clock-circle" />
+      </div>
+      <div className="value--wrapper">
+        {_value}
+      </div>
     </div>
-    <div className="value--wrapper">
-      {moment(value).format(TIME_FORMAT) || 'N/A'}
-    </div>
-  </div>
-);
+  );
+}
 
 TimePicker.propTypes = {
   value: PropTypes.string,
