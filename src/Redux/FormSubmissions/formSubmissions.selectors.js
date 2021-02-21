68437be6eb3a90b7f199f97341310007cae0368e
Andreas Kjellqvist <linco95@gmail.com>
2021-02-21 17:39:18 +0100

    Fixed missing nullcheck in getsectionvalues

--- src/Redux/FormSubmissions/formSubmissions.selectors.js
+++ src/Redux/FormSubmissions/formSubmissions.selectors.js
@@ -44,11 +44,13 @@ const extractValuesFromSectionData = sectionData => {
     case sectionType.CONNECTED:
       return Object.values(sectionData).reduce((values, event) => [
         ...values,
-        ...event.values.reduce((elVals, element) =>
-          [
-            ...elVals,
-            ...element.value
-          ], [])
+        ...event.values
+          .filter((element) => element.value)
+          .reduce((elVals, element) =>
+            [
+              ...elVals,
+              ...element.value
+            ], [])
       ], []);
     default:
       return [];
