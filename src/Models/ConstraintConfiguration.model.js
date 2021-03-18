e90fef24dba638ecad23b7a1b2d937f0c47b5767
Axel Karlsson <axel.karlsson@timeedit.com>
2021-03-18 23:01:12 +0100

    DEV-7595: Added constraintConfigurationId and userId to constraintConfigurationModel

--- src/Models/ConstraintConfiguration.model.js
+++ src/Models/ConstraintConfiguration.model.js
@@ -1,14 +1,26 @@
 export class ConstraintConfiguration {
+  constraintConfigurationId;
   formId;
   name;
   description;
   constraints;
   timestamps;
-  constructor ({ formId, name, description, constraints, timestamps }) {
+  userId;
+  constructor ({
+    formId,
+    name,
+    description,
+    constraints,
+    timestamps,
+    constraintConfigurationId,
+    userId
+  }) {
     this.formId = formId;
     this.name = name;
     this.description = description;
     this.constraints = constraints || [];
     this.timestamps = timestamps;
+    this.constraintConfigurationId = constraintConfigurationId;
+    this.userId = userId;
   }
 }
