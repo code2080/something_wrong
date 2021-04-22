ac65d42cd61e61644ab4be5d6588ab35cc7678db
Andreas Kjellqvist <linco95@gmail.com>
2021-04-22 18:33:48 +0200

    DEV-8074: Improved logic for avoiding to call getExtIdProps if we already have the labels

--- src/Hooks/TECoreApiHooks.ts
+++ src/Hooks/TECoreApiHooks.ts
@@ -24,22 +24,25 @@ export const fetchLabelsFromExtIds = (
   extIds: string[],
   payload: GetExtIdPropsPayload,
 ) => {
-  async function exec(payload: GetExtIdPropsPayload) {
-    const nonNullPayload = {
-      objects: _.compact(payload.objects),
-      types: _.compact(payload.types),
-      fields: _.compact(payload.fields),
-    } as GetExtIdPropsPayload;
+  async function exec(nonNullPayload: GetExtIdPropsPayload) {
     const extIdProps = await teCoreAPI.getExtIdProps(nonNullPayload);
     dispatch(setTEDataForValues(extIdProps));
   }
-  const payloadExtids = _.flatMap(payload);
+
+  const nonNullPayload = {
+    objects: _.compact(payload.objects).filter(
+      (obj) => (obj as TEObject).id !== null,
+    ),
+    types: _.compact(payload.types),
+    fields: _.compact(payload.fields),
+  } as GetExtIdPropsPayload;
+  const payloadExtids = _.flatMap(nonNullPayload);
   const missingExtIdsInStore = !payloadExtids.every(
     (extId) =>
       extId && extIds.includes((extId as TEObject).id ?? (extId as string)),
   );
 
-  if (!_.isEmpty(payloadExtids) && missingExtIdsInStore) exec(payload);
+  if (!_.isEmpty(payloadExtids) && missingExtIdsInStore) exec(nonNullPayload);
 };
 
 export const useFetchLabelsFromExtIds = (payload: GetExtIdPropsPayload) => {
