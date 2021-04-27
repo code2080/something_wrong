511c3e47db9503e61e1ff2c3788f16f9fc113217
Andreas Kjellqvist <linco95@gmail.com>
2021-04-27 17:23:19 +0200

    Removed unused useState

--- src/Pages/FormDetail/pages/submissions.detail.page.js
+++ src/Pages/FormDetail/pages/submissions.detail.page.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { useParams } from 'react-router-dom';
