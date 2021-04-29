2037dd76ec6e0b8252dcf16465f504dd9fc4fa01
Andreas Kjellqvist <linco95@gmail.com>
2021-04-29 15:24:49 +0200

    DEV-8282: Added deleting of resrvations when deleting activities from activity designer

--- src/Components/ActivityDesigner/HasReservationsAlert.js
+++ src/Components/ActivityDesigner/HasReservationsAlert.js
@@ -1,12 +1,69 @@
 import PropTypes from 'prop-types';
-import { Alert, Button } from 'antd';
-import { useDispatch } from 'react-redux';
+import { Alert, Button, Popconfirm } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
 
 // ACTIONS
-import { deleteActivities } from '../../Redux/Activities/activities.actions';
+import {
+  deleteActivities,
+  updateActivity,
+} from '../../Redux/Activities/activities.actions';
+import { useTECoreAPI } from '../../Hooks/TECoreApiHooks';
+import { useMemo, useState } from 'react';
+import { makeSelectActivitiesForForm } from '../../Redux/Activities/activities.selectors';
+import { activityStatuses } from '../../Constants/activityStatuses.constants';
 
 const HasReservationsAlert = ({ formId }) => {
   const dispatch = useDispatch();
+  const teCoreAPI = useTECoreAPI();
+  const [visible, setVisible] = useState(false);
+  const selectActivitiesForForm = useMemo(
+    () => makeSelectActivitiesForForm(),
+    [],
+  );
+  const activities = useSelector((state) =>
+    selectActivitiesForForm(state, formId),
+  );
+  const scheduledActivities = useMemo(
+    () =>
+      Object.values(activities)
+        .flat()
+        .filter((a) => a.reservationId && a.activityStatus !== 'NOT_SCHEDULED'),
+    [activities],
+  );
+  const hasScheduledActivities = scheduledActivities.length > 0;
+
+  const handleDeleteActivities = () => {
+    dispatch(deleteActivities(formId));
+  };
+
+  const handleDeleteReservations = (responses = []) => {
+    // Check result parameter to see if everything went well or not
+    responses.forEach((res) => {
+      if (!res?.result?.details) {
+        const updatedActivity = {
+          ...res.activity,
+          schedulingDate: null,
+          activityStatus: activityStatuses.NOT_SCHEDULED,
+          reservationId: null,
+        };
+        dispatch(updateActivity(updatedActivity));
+      }
+    });
+  };
+
+  const handleConfirm = (deleteReservations) => {
+    setVisible(false);
+    deleteReservations &&
+      teCoreAPI.deleteReservations({
+        activities: scheduledActivities,
+        callback: handleDeleteReservations,
+      });
+    handleDeleteActivities();
+  };
+
+  const onDeleteActivities = () => {
+    hasScheduledActivities ? setVisible(true) : handleDeleteActivities();
+  };
 
   return (
     <Alert
@@ -20,13 +77,21 @@ const HasReservationsAlert = ({ formId }) => {
             The form has already been converted to activities with the current
             design. To edit the design, you must first delete the activities.
           </div>
-          <Button
-            size='small'
-            type='link'
-            onClick={() => dispatch(deleteActivities(formId))}
+          <Popconfirm
+            title='This action will delete all activities for this form.
+            You have activities that have been scheduled. Do you also want to delete these reservations?'
+            overlayStyle={{ width: '450px', wordWrap: 'break-word' }}
+            okText='Yes'
+            cancelText='No'
+            visible={visible}
+            getPopupContainer={() => document.getElementById('te-prefs-lib')}
+            onConfirm={() => handleConfirm(true)}
+            onCancel={() => handleConfirm(false)}
           >
-            Delete activities now
-          </Button>
+            <Button size='small' type='link' onClick={onDeleteActivities}>
+              Delete activities now
+            </Button>
+          </Popconfirm>
         </>
       }
     />
