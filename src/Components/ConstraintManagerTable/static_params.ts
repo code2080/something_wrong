3212672628acbdb28ae9089ad23435cb183fc140
Axel Karlsson <axel.karlsson@timeedit.com>
2021-05-27 13:59:12 +0200

    DEV-8644: Added temporary constraints for a demo

---
+++ src/Components/ConstraintManagerTable/static_params.ts
@@ -0,0 +1,19 @@
+export const staticParams = {
+  Room: {
+    Capacity: 'Platser',
+    Equipment: 'Utrustning',
+    Department: 'Avdelning',
+    Floor: 'Våning',
+  },
+  Activity: {
+    Size: 'kapacitet',
+    Id: 'Namn',
+  },
+};
+
+export const staticDesignObj = {
+  Room: [['room']],
+  Activity: [['']],
+};
+
+export default staticParams;
