3a8e8350af552d7f00112e889b2573e88be057c4
Axel Karlsson <axel.karlsson@timeedit.com>
2021-06-22 16:09:19 +0200

    DEV-8862: Broke out joint teaching into different components

--- src/Components/ActivitiesTableColumns/SchedulingColumns/SchedulingActions/JointTeachingIcon.tsx
+++ src/Components/ActivitiesTableColumns/SchedulingColumns/SchedulingActions/JointTeachingIcon.tsx
@@ -1,9 +1,21 @@
 import { RiShareBoxFill } from 'react-icons/ri';
-import { Button, Popover, Modal } from 'antd';
-import { useState } from 'react';
+import { Button, Popover } from 'antd';
+import HoverAndClickPopOver from '../../ActivityValueColumns/Helpers/HoverAndClickPopOver';
+import SelectWithDeleteOption from '../../ActivityValueColumns/Helpers/SelectWithDeleteOption';
+import { useSelector } from 'react-redux';
 
 const JointTeachingIcon = (jointTeachingProps) => {
+  const [jointTeachingObj, setJointTeachingObj] = useSelector();
+
   if (!jointTeachingProps) return;
+  const hoverContent = 'Click to indicate joint teaching';
+  const clickContent = (
+    <SelectWithDeleteOption
+      placeholder={'Placeholder'}
+      header={'Select joint teaching object'}
+    />
+  );
+  const icon = <RiShareBoxFill />;
 
   if (jointTeachingProps?.hasJointTeaching)
     return (
@@ -20,21 +32,16 @@ const JointTeachingIcon = (jointTeachingProps) => {
     );
 
   if (jointTeachingProps?.isMerged)
-    return (
-      <Button icon={<RiShareBoxFill size='large' color='green' />}></Button>
-    );
+    return <Button icon={<RiShareBoxFill size='large' color='green' />} />;
 
   return (
-    <Popover content={'Click to indicate joint teaching'}>
-      <Button
-        style={{
-          padding: 0,
-          border: 'none',
-          background: 'none',
-        }}
-        icon={<RiShareBoxFill size='large' />}
-      ></Button>
-    </Popover>
+    <>
+      <HoverAndClickPopOver
+        hoverContent={hoverContent}
+        clickContent={clickContent}
+        icon={icon}
+      />
+    </>
   );
 };
 
