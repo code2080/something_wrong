c261de8f1ace52d9ea6cd67fa937a9d1c035b6cd
Axel Karlsson <axel.karlsson@timeedit.com>
2021-06-22 16:09:40 +0200

    DEV-8862: Updated name in schedulingAction

--- src/Components/ActivitiesTableColumns/SchedulingColumns/SchedulingActions/SchedulingActions.js
+++ src/Components/ActivitiesTableColumns/SchedulingColumns/SchedulingActions/SchedulingActions.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import SelectActivityButton from './SelectActivityButton';
 import SchedulingCheckbox from './SchedulingCheckbox';
 import ActionsDropdown from './ActionsDropdown';
-import JointTeachingButton from './JointTeachingIcon';
+import JointTeachingIcon from './JointTeachingIcon';
 // CONSTANTS
 import { activityStatuses } from '../../../../Constants/activityStatuses.constants';
 
@@ -19,7 +19,7 @@ const SchedulingActions = ({ activity }) => {
       )}
       <SchedulingCheckbox activity={activity} />
       <SelectActivityButton activity={activity} />
-      <JointTeachingButton />
+      <JointTeachingIcon />
       <ActionsDropdown buttonType='ellipsis' activity={activity} />
     </div>
   );
