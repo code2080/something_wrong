f99780fe84043181e4bb0ff1b10a5c65ce68823d
Andreas Kjellqvist <linco95@gmail.com>
2021-06-30 13:51:32 +0200

    APP-40: Added redux wrapper for testing

---
+++ src/Utils/test.utils.tsx
@@ -0,0 +1,19 @@
+import { render as rtlRender, RenderOptions } from '@testing-library/react';
+import { JSXElementConstructor, ReactElement, ReactNode } from 'react';
+import { Provider } from 'react-redux';
+import configureStore from '../Redux/store';
+
+export const renderWithState = (
+  ui: ReactElement<any, string | JSXElementConstructor<any>>,
+  {
+    initialState = {},
+    ...renderOptions
+  }: RenderOptions & { initialState?: any } = {},
+) => {
+  const store = configureStore(initialState);
+  const Wrapper = ({ children }: { children?: ReactNode }) => (
+    <Provider store={store}>{children}</Provider>
+  );
+
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+};
