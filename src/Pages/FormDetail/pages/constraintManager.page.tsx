022a33d31c7cc0d329e65f843bb46ca4a3e6aac2
Andreas Kjellqvist <linco95@gmail.com>
2021-08-24 14:11:05 +0200

    Removed unused add custom constraint button

--- src/Pages/FormDetail/pages/constraintManager.page.tsx
+++ src/Pages/FormDetail/pages/constraintManager.page.tsx
@@ -176,10 +176,6 @@ const ConstraintManagerPage = () => {
     ],
   );
 
-  const handleAddCustomConstraint = (e) => {
-    e.stopPropagation();
-  };
-
   const handleCreateConstrConf = useCallback(() => {
     const newConstrConf = ConstraintConfiguration.create({
       formId,
@@ -251,15 +247,7 @@ const ConstraintManagerPage = () => {
             />
           </Collapse.Panel>
           {hasAEBetaPermission && (
-            <Collapse.Panel
-              key='CUSTOM'
-              header='Custom constraints'
-              extra={
-                <Button onClick={handleAddCustomConstraint} size='small'>
-                  Add new custom constraint
-                </Button>
-              }
-            >
+            <Collapse.Panel key='CUSTOM' header='Custom constraints'>
               <Table
                 columns={constraintManagercolumns}
                 dataSource={customConstraints}
