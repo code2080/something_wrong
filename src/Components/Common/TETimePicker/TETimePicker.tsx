3c45c76174f0b26e74f2f9ce6a32424fdd6a614e
Andreas Kjellqvist <linco95@gmail.com>
2021-08-27 23:06:12 +0200

    APP-117: removed all import react default as it's not needed in react 17

--- src/Components/Common/TETimePicker/TETimePicker.tsx
+++ src/Components/Common/TETimePicker/TETimePicker.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import { useRef } from 'react';
 
 import { TimePicker, TimePickerProps } from 'antd';
 import { Moment } from 'moment';
--- src/Components/Common/TETimePicker/TETimeRangePicker.tsx
+++ src/Components/Common/TETimePicker/TETimeRangePicker.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import { useRef } from 'react';
 
 import { TimePicker, TimeRangePickerProps } from 'antd';
 
