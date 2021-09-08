0e8cd749c3f32a6e1ea4de372471ccf022a94253
Andreas Kjellqvist <linco95@gmail.com>
2021-09-08 16:19:57 +0200

    APP-65: Added parameter data to constraintconfiguration model

--- src/Models/ConstraintConfiguration.model.js
+++ src/Models/ConstraintConfiguration.model.js
@@ -4,11 +4,13 @@ export class ConstraintConfiguration {
   name;
   description;
   constraints;
-  constructor({ _id, formId, name, description, constraints }) {
+  parameterData;
+  constructor({ _id, formId, name, description, constraints, parameterData }) {
     this._id = _id;
     this.formId = formId;
     this.name = name;
     this.description = description;
     this.constraints = constraints || [];
+    this.parameterData = parameterData || [];
   }
 }
