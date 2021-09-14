56b1872cfa89a052eb8080f71b141c93d5f983c9
Andreas Kjellqvist <linco95@gmail.com>
2021-09-14 17:58:49 +0200

    DEV-9439: Added tests for labels in activity object value rendering

--- src/Components/ActivitiesTableColumns/ActivityValueColumns/ValueTypes/__tests__/ObjectObjectValue.tests.tsx
+++ src/Components/ActivitiesTableColumns/ActivityValueColumns/ValueTypes/__tests__/ObjectObjectValue.tests.tsx
@@ -1,8 +1,31 @@
+import { screen } from '@testing-library/dom';
 import ObjectObjectValue from '../ObjectObjectValue';
 import { renderWithState } from '../../../../../Utils/test.utils';
 
 describe('ObjectObjectValue component tests', () => {
   it('renders with empty parameters', () => {
     renderWithState(<ObjectObjectValue value={[]} />);
+  });
+
+  it('renders labels when labels are available', () => {
+    renderWithState(<ObjectObjectValue value={['testExtId']} />, {
+      initialState: {
+        te: {
+          extIdProps: {
+            objects: {
+              testExtId: {
+                label: 'TestExtIdLabel',
+              },
+            },
+          },
+        },
+      },
+    });
+    screen.getByText('TestExtIdLabel');
+  });
+
+  it('renders extId when labels are unavailable', () => {
+    renderWithState(<ObjectObjectValue value={['testExtId']} />);
+    screen.getByText('testExtId');
   });
 });
