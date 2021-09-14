59354df62d141113cfa4de6f580f4a3c06fbe71e
Andreas Kjellqvist <linco95@gmail.com>
2021-09-14 17:58:05 +0200

    APP-246: Added tests for disabling timing modes

---
+++ src/Components/ActivityDesigner/__tests__/TimingMapping.test.tsx
@@ -0,0 +1,78 @@
+import noop from 'lodash/noop';
+import { fireEvent, screen, waitFor } from '@testing-library/dom';
+import last from 'lodash/last';
+import TimingMapping from '../TimingMapping';
+import { renderWithState } from '../../../Utils/test.utils';
+import { activityTimeModes } from '../../../Constants/activityTimeModes.constants';
+
+describe('TimingMapping tests', () => {
+  const expectAntOption = (optionElement?: HTMLElement) => {
+    if (!optionElement) {
+      fail('Could not find element');
+    }
+    const antdSelectOptionClass = 'ant-select-item-option';
+
+    const optionButtonDiv = Object.values(
+      optionElement.closest(`.${antdSelectOptionClass}`)?.classList ?? {},
+    );
+
+    const expectedValue = expect(optionButtonDiv);
+    return {
+      not: {
+        toBeDisabled: () =>
+          expectedValue.not.toContain(`${antdSelectOptionClass}-disabled`),
+      },
+      toBeDisabled: () =>
+        expectedValue.toContain(`${antdSelectOptionClass}-disabled`),
+    };
+  };
+
+  const minimumProps = {
+    onChange: noop,
+    formSections: [],
+    mapping: { timing: { mode: activityTimeModes.SEQUENCE } },
+  };
+  it('minimal props for rendering', () => {
+    renderWithState(<TimingMapping {...minimumProps} />);
+  });
+
+  it('allows all modes if calendar section is available', async () => {
+    renderWithState(
+      <TimingMapping
+        {...minimumProps}
+        formSections={[{ isConnected: true }]}
+        mapping={{ ...minimumProps.mapping, objects: {}, fields: {} }}
+      />,
+    );
+
+    const seqButton = screen.getByText('Sequence');
+    fireEvent.mouseDown(seqButton);
+
+    await waitFor(() => screen.getByText('Exact'));
+    expectAntOption(screen.getByText('Exact')).not.toBeDisabled();
+    expectAntOption(screen.getByText('Timeslots')).not.toBeDisabled();
+    expectAntOption(last(screen.getAllByText('Sequence'))).not.toBeDisabled();
+  });
+
+  it('only allows sequence mode if calendar section is not available', async () => {
+    renderWithState(
+      <TimingMapping
+        {...minimumProps}
+        formSections={[{ isConnected: false, elements: [] }]}
+        mapping={{
+          ...minimumProps.mapping,
+          objects: {},
+          fields: {},
+        }}
+      />,
+    );
+
+    const seqButton = screen.getByText('Sequence');
+    fireEvent.mouseDown(seqButton);
+
+    await waitFor(() => screen.getByText('Exact'));
+    expectAntOption(screen.getByText('Exact')).toBeDisabled();
+    expectAntOption(screen.getByText('Timeslots')).toBeDisabled();
+    expectAntOption(last(screen.getAllByText('Sequence'))).not.toBeDisabled();
+  });
+});
