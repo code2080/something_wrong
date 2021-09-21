2d4eebd881077a83f2ea1c8d2ca58c3b2958c18a
Andreas Kjellqvist <linco95@gmail.com>
2021-09-21 17:21:19 +0200

    Fixed crash on opening activities tab after mapping fields to object filter

--- src/Components/ActivitiesTableColumns/ActivityValueColumns/ValueTypes/FieldValue.jsx
+++ src/Components/ActivitiesTableColumns/ActivityValueColumns/ValueTypes/FieldValue.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 
 const FieldValue = ({ value }) => {
+  if (!Array.isArray(value) && value instanceof Object) return null;
   const formattedValue = Array.isArray(value) ? value.join(', ') : value;
   if (formattedValue && typeof formattedValue === 'object')
     return JSON.stringify(formattedValue);
