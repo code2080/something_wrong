23f320aa10630131152b13c20bcb1c4aa45113dd
Axel Karlsson <axel.karlsson@timeedit.com>
2021-11-12 10:15:38 +0100

    APP-113: Added selectors for fetching actvities by formId and formInstanceId

--- src/Redux/ActivityScheduling/activityScheduling.selectors.ts
+++ src/Redux/ActivityScheduling/activityScheduling.selectors.ts
@@ -3,23 +3,25 @@ import { createSelector } from 'reselect';
 import { TActivity } from 'Types/Activity.type';
 import { ActivitySchedulingState } from './activityScheduling.reducer';
 
-const stateSelector = (state): ActivitySchedulingState =>
-  state.activityScheduling;
-const activityStateSelector = (state) => state.activities;
+const activitySchedulingStateSelector = (state: {
+  activityScheduling: ActivitySchedulingState;
+}): ActivitySchedulingState => state.activityScheduling;
+
+const activityStateSelector = (state: any) => state.activities || {};
 
 export const selectActivityScheduling = () =>
   createSelector(
-    stateSelector,
+    activitySchedulingStateSelector,
     (activityScheduling) => activityScheduling.scheduling,
   );
 export const selectActivitySchedulingById = (activityId: string) =>
   createSelector(
-    stateSelector,
+    activitySchedulingStateSelector,
     (activityScheduling) => activityScheduling.scheduling[activityId],
   );
 export const selectAllActivitiesAreScheduling = (formId: string) =>
   createSelector(
-    stateSelector,
+    activitySchedulingStateSelector,
     activityStateSelector,
     (activityScheduling, activity) => {
       // TODO: SSP: This doesnt work with SSP
@@ -36,4 +38,30 @@ export const selectAllActivitiesAreScheduling = (formId: string) =>
       ).filter((key) => activityScheduling.scheduling[key]);
       return isEmpty(difference(allActivityIds, schedulingActivityIds));
     },
+  );
+
+export const makeSelectAllActivityIds = () => {
+  createSelector(
+    activityStateSelector,
+    (_: any, formId: string) => formId,
+    (uiState, formId) => uiState.paginationParams[formId] || {},
+  );
+};
+
+export const makeSelectAllActivityIdsForForm = () =>
+  createSelector(
+    activityStateSelector,
+    (_: any, formId: string) => formId,
+    (activities, formId: string) => activities?.byFormId?.[formId] ?? [],
+  );
+
+export const makeSelectAllActivityidsForForminstance = () =>
+  createSelector(
+    activityStateSelector,
+    (_: any, formId: string, formInstanceId: string) => ({
+      formId,
+      formInstanceId,
+    }),
+    (activities, { formId, formInstanceId }) =>
+      activities?.byFormInstanceId?.[formId]?.[formInstanceId],
   );
