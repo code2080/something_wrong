951e8e4ef1f473bcbdce1f42fd391f07e316f7d0
friendken <friendkenten@gmail.com>
2022-01-21 16:06:31 +0000

    APP-576: fix: using forceActivity action to refresh the activity data after woker progress is done

--- src/Hooks/useActivityGeneratorWSAPI.ts
+++ src/Hooks/useActivityGeneratorWSAPI.ts
@@ -4,10 +4,8 @@ import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getEnvParams } from '../configs';
 
-import {
-  updateActivityInWorkerProgress,
-} from '../Redux/Activities/activities.actions';
-import { forceFetchingActivities } from 'Redux/GlobalUI/globalUI.actions'
+import { updateActivityInWorkerProgress } from '../Redux/Activities/activities.actions';
+import { forceFetchingActivities } from 'Redux/GlobalUI/globalUI.actions';
 import { ACTIVITIES_TABLE } from 'Constants/tables.constants';
 
 const useActivityGeneratorWSAPI = () => {
@@ -21,7 +19,8 @@ const useActivityGeneratorWSAPI = () => {
     if (formId) {
       socket.emit('activityGeneration', { formId });
       socket.on('activityGeneration', ({ status, formId }) => {
-        if (status === 'DONE') dispatch(forceFetchingActivities(ACTIVITIES_TABLE));
+        if (status === 'DONE')
+          dispatch(forceFetchingActivities(ACTIVITIES_TABLE));
         dispatch(updateActivityInWorkerProgress({ formId }));
       });
     }
