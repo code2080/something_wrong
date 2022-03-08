d948dad6aa13eb0e9f2674d7628db86a643ae0ad
Johan Hörnell <johan.hornell@timeedit.com>
2022-03-08 08:31:27 -0500

    updated State typing

--- src/Types/State.type.ts
+++ src/Types/State.type.ts
@@ -1,9 +1,8 @@
-// import { IDefaultPaginatedState } from "./SSP.type";
 import {TActivityTag} from "./ActivityTag.type"
+import { ISSPReducerState } from "./SSP.type";
 
 export interface IState {
-  // activitiesNew: IDefaultPaginatedState;
-  activitiesNew: any;
+  activitiesNew: ISSPReducerState;
   integration: {
     mappedObjectsLabel: any;
     mappedObjectTypes: any;
