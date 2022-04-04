05a95a03aead6deb96875a3c02a3188abd8cf11b
friendken <friendkenten@gmail.com>
2022-04-04 17:22:50 +0100

    bugfix: APP-936 Error when updating date interval filter for activities

--- src/Components/ActivitySSPFilters/Components/DateFilterItem/index.tsx
+++ src/Components/ActivitySSPFilters/Components/DateFilterItem/index.tsx
@@ -1,4 +1,5 @@
 import { DatePicker, Form } from 'antd';
+import moment from 'moment';
 
 // TYPES
 type Props = {
@@ -7,13 +8,23 @@ type Props = {
 };
 
 const DateFilterItem = ({ onChange, value }: Props) => {
+  let transform = value;
+
+  // transform string to moment object if the value is already configured
+  if (value) {
+    transform = [
+      value[0] ? moment(value[0]) : null,
+      value[1] ? moment(value[1]) : null,
+    ];
+  }
+
   return (
     <Form.Item label='Select date interval'>
       <DatePicker.RangePicker
         allowEmpty={[true, true]}
         minuteStep={30}
         onChange={onChange}
-        value={value}
+        value={transform}
         allowClear={false}
       />
     </Form.Item>
